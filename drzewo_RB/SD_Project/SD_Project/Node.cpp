7eace70745cec7487d95f3237366d26fb0015ec4
shoshano <shoshano2048@gmail.com>
2023-05-08 18:18:58 +0200

    RB Node cpp+h

---
+++ drzewo_RB/SD_Project/SD_Project/Node.cpp
@@ -0,0 +1,53 @@
+#include "Node.h"
+
+template<class T>
+void Node<T>::setValue(T value) {
+	this->value = value;
+}
+
+template<class T>
+void Node<T>::setColor(bool color) {
+	this->color = color;
+}
+
+
+template<class T>
+void Node<T>::setParent(Node<T> *parent) {
+	this->parent = parent;
+}
+
+template<class T>
+void Node<T>::setLeft(Node<T>* left) {
+	this->left = left;
+}
+
+template<class T>
+void Node<T>::setRight(Node<T>* right) {
+	this->right = right;
+}
+
+template<class T>
+T Node<T>::getValue() {
+	return value;
+}
+
+template<class T>
+bool Node<T>::getColor() {
+	return color;
+}
+
+template<class T>
+Node<T>* Node<T>::getParent() {
+	return parent;
+}
+template<class T>
+Node<T>* Node<T>::getLeft() {
+	return left;
+}
+
+template<class T>
+Node<T>* Node<T>::getRight() {
+	return right;
+}
+
+template class Node<int>;
---
+++ drzewo_RB/SD_Project/SD_Project/SD_Project.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Projekt struktury danych\n";
+}
+
