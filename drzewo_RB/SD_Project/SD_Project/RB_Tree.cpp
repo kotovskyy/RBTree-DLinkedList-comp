193151bc2794716f429c07e46167ce82583fd322
shoshano <shoshano2048@gmail.com>
2023-05-15 19:54:29 +0200

    finish RB_Tree class header file

--- drzewo_RB/SD_Project/SD_Project/RB_Tree.cpp
+++ drzewo_RB/SD_Project/SD_Project/RB_Tree.cpp
@@ -1,10 +1,46 @@
 #include "RB_Tree.h"
+#include "Node.h"
 
 template<class T>
-void RB_Tree<T>::add(){}
+RB_Tree<T>::RB_Tree() {
+	root = nullptr;
+}
 
 template<class T>
-void RB_Tree<T>::remove() {}
+RB_Tree<T>::~RB_Tree() {}
 
 template<class T>
-void RB_Tree<T>::search() {}
+void RB_Tree<T>::insert(Node<T>* node){
+	
+}
+
+template<class T>
+void RB_Tree<T>::remove(Node<T>* node) {
+
+}
+
+template<class T>
+void RB_Tree<T>::search(T key) {
+
+
+}
+
+template<class T>
+void RB_Tree<T>::leftRotation(Node<T>* x) {
+
+}
+
+template<class T>
+void RB_Tree<T>::rightRotation(Node<T>* x) {
+
+}
+
+template<class T>
+void RB_Tree<T>::fixAfterInsert(Node<T>* x) {
+
+}
+
+template<class T>
+void RB_Tree<T>::fixAfterRemove(Node<T>* x) {
+
+}
--- drzewo_RB/SD_Project/SD_Project/RB_Tree.h
+++ drzewo_RB/SD_Project/SD_Project/RB_Tree.h
@@ -1,13 +1,21 @@
 #ifndef RB_TREE_H_
 #define RB_TREE_H_
+#include "Node.h"
 
 template<class T>
 class RB_Tree {
-
+	Node<T>* root;
 public:
-	void add();
-	void remove();
-	void search();
+	RB_Tree();						// konstruktor
+	~RB_Tree();						// dekonstruktor
+
+	void insert(Node<T> * node);	// dodaje 
+	void remove(Node<T>* node);		// usuwa
+	void search(T key);				// szuka
+	void leftRotation(Node<T>* x);	// rotuje w lewo
+	void rightRotation(Node<T>* x);	// rotuje w prawo
+	void fixAfterInsert(Node<T>* x);			// napiawia po dodaniu
+	void fixAfterRemove(Node<T>* x);			// naprawia po usuwaniu
 
 };
 
